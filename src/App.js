fd4c99ebe805cab20a55b3e9f2b02380eb76eba7
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2019-03-28 20:25:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    pokemons: [],
+    name: {},
+    status: "loading"
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then(({ data: { results: pokemons } }) => {
+        this.setState({
+          pokemons,
+          status: "loaded"
+        });
+      })
+      .catch(error => {
+        console.log("error", error);
+        this.setState({
+          status: "error"
+        });
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    console.log("render");
+    switch (this.state.status) {
+      case "loading":
+        return "loading...";
+      case "loaded":
+        return (
+          <div className="App">
+            <h1>Pokemons</h1>
+            {this.state.pokemons.map(pokemon => {
+              return <li key={pokemon.name}>{pokemon.name}</li>;
+            })}
+          </div>
+        );
+      case "error":
+        return "vaya me espiche!!!! ";
+      default:
+        break;
+    }
   }
 }
 
